839b8344b3dcc6df9feaa308ee3835dbad6795f3
Fabrizio Arias <fabriidev@gmail.com>
2025-03-02 20:54:20 -0300

    correccion de formulario
    dejo funcionando el formulario

--- src/components/sub.components/FormContacto.jsx
+++ src/components/sub.components/FormContacto.jsx
@@ -7,30 +7,17 @@ const FormContacto = () => {
   const [email, setEmail] = useState("");
   const [mensaje, setMensaje] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!nombre || !email || !mensaje) {
-      alert("Por favor, completa todos los campos.");
-      return;
-    }
-
-    setLoading(true);
-    setEnviado(true);
-
-    // Aquí iría el código para enviar el formulario si se estuviera utilizando una API
-  };
-
   return (
     <div className="flex justify-center items-center mt-2 bg-gray-900">
       <form
         action="https://formsubmit.co/fabriidev@gmail.com"
         method="POST"
         className="bg-gray-800 p-6 rounded-xl shadow-lg w-96"
-        onSubmit={handleSubmit}
+        onSubmit={() => setLoading(true)}
       >
-        <input type="hidden" name="_next" value="http://localhost:5173/" />
+        {/* Campos ocultos para FormSubmit */}
         <input type="hidden" name="_captcha" value="false" />
+        <input type="hidden" name="_next" value="https://fabrizio-arias.vercel.app/" />
 
         <label className="block text-white mb-1">Nombre:</label>
         <input
@@ -38,11 +25,8 @@ const FormContacto = () => {
           name="nombre"
           value={nombre}
           onChange={(e) => setNombre(e.target.value)}
-          className={`w-full p-2 mb-4 border border-pink-500 rounded bg-gray-700 text-white ${
-            (loading || enviado) && "bg-gray-600 opacity-50 cursor-not-allowed"
-          }`}
+          className="w-full p-2 mb-4 border border-pink-500 rounded bg-gray-700 text-white"
           required
-          disabled={loading || enviado}
         />
 
         <label className="block text-white mb-1">Email:</label>
@@ -51,11 +35,8 @@ const FormContacto = () => {
           name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className={`w-full p-2 mb-4 border border-pink-500 rounded bg-gray-700 text-white ${
-            (loading || enviado) && "bg-gray-600 opacity-50 cursor-not-allowed"
-          }`}
+          className="w-full p-2 mb-4 border border-pink-500 rounded bg-gray-700 text-white"
           required
-          disabled={loading || enviado}
         />
 
         <label className="block text-white mb-1">Mensaje:</label>
@@ -63,20 +44,16 @@ const FormContacto = () => {
           name="mensaje"
           value={mensaje}
           onChange={(e) => setMensaje(e.target.value)}
-          className={`w-full p-2 mb-4 border border-pink-500 rounded bg-gray-700 text-white ${
-            (loading || enviado) && "bg-gray-600 opacity-50 cursor-not-allowed"
-          }`}
+          className="w-full p-2 mb-4 border border-pink-500 rounded bg-gray-700 text-white"
           required
-          disabled={loading || enviado}
         />
 
         <button
           type="submit"
           className="bg-pink-500 text-white p-2 w-full rounded hover:bg-pink-700 transition duration-300 flex justify-center disabled:bg-gray-600 disabled:opacity-50 disabled:cursor-not-allowed"
           disabled={loading || enviado}
-          style={{ cursor: loading || enviado ? "not-allowed" : "pointer" }}
         >
-          Enviar
+          {loading ? "Enviando..." : "Enviar"}
         </button>
 
         {enviado && (
